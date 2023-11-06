d83a3445251fb1c3afa7d1f93b3dbefb7db29957
safdar211ali <safdar211@hotmail.com>
2023-11-06 07:38:07 +0500

    updated

--- kdlb/billing/report/tues_out_summary_modified/tues_out_summary_modified.py
+++ kdlb/billing/report/tues_out_summary_modified/tues_out_summary_modified.py
@@ -108,7 +108,7 @@ def get_data(filters):
                     FROM 
                         `tabGL Entry`
                     WHERE 
-                        credit > 0
+                        `voucher_type` = 'Payment Entry'
                     GROUP BY 
                         `against_voucher`
                 ) AS `tabGL Entry Aggregated`
